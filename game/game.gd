6c8ea1d4aaa6dcefc0c6127d1f4206e4299f3110
DAAAAANIIIIIIIIII <73837003+D4NIFAKTOR1ELLE@users.noreply.github.com>
2026-02-28 20:39:06 +0100

    Extra

--- game/game.gd
+++ game/game.gd
@@ -2,8 +2,21 @@ extends Node
 
 @onready var gametimer: Timer = $GameTimer
 @onready var music: AudioStreamPlayer = $Music
+@onready var timer_text: Label = $TruckInside/Control/Time
 
 func _ready() -> void:
 	print("Game launched!")
 	
 	gametimer.start()
+
+func lose():
+	pass
+	
+func win():
+	pass
+
+func _on_game_timer_timeout() -> void:
+	lose()
+
+func _process(delta: float) -> void:
+	timer_text.set_text("%02d:%02d" % [gametimer.time_left / 60, fmod(gametimer.time_left, 60)])
