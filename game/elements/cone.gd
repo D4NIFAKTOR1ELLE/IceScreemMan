75f7b429307b036b98ae9c9fcadc6f7ba709c023
DAAAAANIIIIIIIIII <73837003+D4NIFAKTOR1ELLE@users.noreply.github.com>
2026-03-01 09:11:40 +0100

    Cone is fully functional now with adding and removing scoops

--- game/elements/cone.gd
+++ game/elements/cone.gd
@@ -3,11 +3,11 @@ extends Control
 class_name Cone
 
 var scoops: int = 0
-@onready var start_scoop = $"Scoops/1"
+@onready var start_scoop = $"Scoops/0"
 
 func initialise():
 	set_physics_process(true)
-
+	
 func _physics_process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
 
@@ -16,16 +16,18 @@ func add_scoop(colour: Color):
 		return
 	scoops = scoops + 1
 	var new_scoop = start_scoop.duplicate()
-	new_scoop.name = str(scoops + 1)
+	new_scoop.name = str(scoops)
 	
 	new_scoop.self_modulate = colour
+	new_scoop.visible = true
 	$Scoops.add_child(new_scoop)
-	new_scoop.position = Vector2(start_scoop.position.x, get_node("Scoops/%s" % scoops).position.y - 30)
+	new_scoop.position = Vector2(start_scoop.position.x, get_node("Scoops/%s" % str(scoops - 1)).position.y - 30)
 
 func remove_scoop():
 	if scoops == 0:
 		delete()
 	
+	get_node("Scoops/%s" % str(scoops)).queue_free()
 	scoops = scoops - 1
 
 func _input(_event: InputEvent) -> void:
@@ -33,5 +35,7 @@ func _input(_event: InputEvent) -> void:
 		delete()
 
 func delete():
-	#TODO This'll check if the cone is full before deleting itself
-	self.queue_free()
+	if scoops > 0:
+		remove_scoop()
+	else:
+		self.queue_free()
