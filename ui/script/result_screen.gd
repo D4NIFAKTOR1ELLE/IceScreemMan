95802dd418a65cc25048a6e72965878282bc21a4
Ludwiguwo <92888923+Ludwiguwo@users.noreply.github.com>
2026-03-02 21:42:58 +0100

    ResultScreen Star Formula, please test

--- ui/script/result_screen.gd
+++ ui/script/result_screen.gd
@@ -3,7 +3,24 @@ extends CanvasLayer
 func load_data():
 	$TextureRect/MarginContainer/VBoxContainer/SanityContainer/Score.text = "%d / %d" % [Constants.sanity, Constants.max_sanity] 
 	$TextureRect/MarginContainer/VBoxContainer/TimerContainer/Score.text = "%02d:%02d" % [Game.gametimer.time_left / 60, fmod(Game.gametimer.time_left, 60)]
+	$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label2.text = "%d / 3 Rats" [calculate_stars()]
+	update_stars()
+	
+func update_stars() -> void:
+	if calculate_stars() < 2:
+		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label2.modulate = Color(0.3, 0.3, 0.3)
+		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label3.modulate = Color(0.3, 0.3, 0.3)
+	else:
+		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label3.modulate = Color(0.3, 0.3, 0.3)
 
+func calculate_stars() -> int: # 1 Stern fuer Gewinnen, 1 Stern fuer alle Sanity und 1 Stern fuer mehr als 2 min Zeit left
+	var stars = 1
+	if Constants.sanity == Constants.max_sanity:
+		stars += 1
+	if Game.gametimer.time_left > 120:
+		stars += 1
+	return stars
+	
 func _on_button_pressed() -> void:
 	Transition.fade_in()
 	await Transition.transition_anim.animation_finished
