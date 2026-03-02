29abaf5bb0a0f6e2ee252515ee68f8b0f2bc236b
Ludwiguwo <92888923+Ludwiguwo@users.noreply.github.com>
2026-03-02 21:51:24 +0100

    Update result_screen.gd

--- ui/script/result_screen.gd
+++ ui/script/result_screen.gd
@@ -7,10 +7,10 @@ func load_data():
 	update_stars()
 	
 func update_stars() -> void:
-	if calculate_stars() < 2:
+	if calculate_stars() == 1:
 		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label2.modulate = Color(0.3, 0.3, 0.3)
 		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label3.modulate = Color(0.3, 0.3, 0.3)
-	else:
+	elif calculate_stars() == 2:
 		$TextureRect/MarginContainer/VBoxContainer/ScoreContainer/Label3.modulate = Color(0.3, 0.3, 0.3)
 
 func calculate_stars() -> int: # 1 Stern fuer Gewinnen, 1 Stern fuer alle Sanity und 1 Stern fuer mehr als 2 min Zeit left
